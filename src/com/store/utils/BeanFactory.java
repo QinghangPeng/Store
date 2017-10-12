a68f95cf298d436f1e4d22e4c47c0f745ceadac6
jackson <jackson@jackson-PC>
2017-10-12 22:44:55 +0800

    【加强添加方法】

--- src/com/store/utils/BeanFactory.java
+++ src/com/store/utils/BeanFactory.java
@@ -1,7 +1,10 @@
 package com.store.utils;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 import org.dom4j.Document;
-import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
@@ -27,7 +30,27 @@ public class BeanFactory {
 			//获取bean对象的class属性
 			String value = ele.attributeValue("class");
 			//反射
-			return Class.forName(value).newInstance();
+			//return Class.forName(value).newInstance();
+			//对service中的add操作进行加强，打印操作日志
+			final Object object = Class.forName(value).newInstance();
+			if(id.endsWith("Service")) {
+				Object proxyObj = Proxy.newProxyInstance(object.getClass().getClassLoader(), object.getClass().getInterfaces(), new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+						//继续判断是否调用的add或者regist
+						if("add".equals(method.getName()) || "regist".equals(method.getName())){
+							System.out.println("添加操作");
+							return method.invoke(object, args);
+						}
+						
+						return method.invoke(object, args);
+					}
+				});
+				
+				//若是service方法返回的是代理对象
+				return proxyObj;
+			}
+			return object;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
